bd1f7298c5d923645dfe24751fbca8b979ca7900
Yutaro Sato <tanuki.misscast@gmail.com>
2017-08-08 12:43:30 +0000

    edit bootstrap in crate

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,10 +5,8 @@
 <h1>新規タスクの追加ページ</h1>
 
 <div class="row">
-  <div class="col-xs-12">
-  <div class="col-sm-offset-2 col-sm-8">
-  <div class="col-md-offset-2 col-md-8">
-  <div class="col-lg-offset-3 col-lg-6">
+  <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+  
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
       <div class="form-group">
@@ -24,9 +22,7 @@
         {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-  </div>
-  </div>
-  </div>
+  
   </div>
 </div>
 @endsection
