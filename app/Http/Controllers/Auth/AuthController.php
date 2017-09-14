1013990a5d579063a84b56e7c9552cff9a16ac56
Yutaro Sato <tanuki.misscast@gmail.com>
2017-09-14 07:39:48 +0000

    user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -31,6 +31,7 @@ class AuthController extends Controller
     
     // 追加
     protected $redirectTo = '/';
+    protected $loginPath = '/login';    // 追加
     
     public function __construct()
     {
